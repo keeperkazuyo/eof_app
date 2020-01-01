45c665a908153a5d69a716d80a0e6a553825d301
EC2 Default User <ec2-user@ip-172-31-47-170.ap-northeast-1.compute.internal>
2020-01-01 23:13:05 +0900

    ツイッターログインロールバックURL明示

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], callback_url: "http://f-ist.club/users/auth/twitter/callback"
+  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], callback_url: "http://f-ist.club/users/auth/twitter"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
