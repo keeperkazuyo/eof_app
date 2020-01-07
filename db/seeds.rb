90813c3933757eceb54d5b1ffe6c2f4df4178fad
EC2 Default User <ec2-user@ip-172-31-47-170.ap-northeast-1.compute.internal>
2020-01-07 22:44:02 +0900

    APIkey記述方法編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
-    email: "1202871215053893632-twitter@example.com",
-    uid: "1202871215053893632",
-    provider: "twitter",
-    name: "フィストクラブ-格闘技ファンサイト-",
-    nickname: "fist_club2019",
-    location: "",
-    image: "http://pbs.twimg.com/profile_images/1206375717430157314/5sR9BJls_normal.jpg"
-    )
+
