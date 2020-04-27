9019b94739aa5edfc91ef3da8f36f8443aa15172
EC2 Default User <ec2-user@ip-172-31-47-170.ap-northeast-1.compute.internal>
2020-04-27 17:59:57 +0900

    ヘルパーメソッド追加・修正（ページタイトル）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     
-    def full_title(page_title)
-        base_title = "FIST CLUBE-格闘技ファンサイト-"
+    def full_title(page_title = "")
+        base_title = "FIST CLUB-格闘技ファンサイト-"
         if page_title.empty?
             base_title
         else
-            "#{page_title} | #{base_title}"
+            page_title + "|" + base_title
         end
     end
     
